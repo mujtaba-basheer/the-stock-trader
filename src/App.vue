c2ef5d9bec49772c381c934c6ffb7304b0c3fc39
mujtaba-basheer <mujtababasheer14@gmail.com>
2019-11-29 20:39:16 +0530

    Created the first components

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+      
+    </div>
+</template>
+
+<script>
+
+    export default {
+      data() {
+        return {
+          status: 'Critical'
+        }
+      },
+    }
+</script>
+
+<style>
+
+</style>
